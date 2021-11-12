71c7bc59e556a35afe59d496fe14439eee968e7b
Laurent Vleminckx <laurentvleminckx@gmail.com>
2021-11-12 17:25:45 +0100

    Add Immoweb theme color

--- client/src/components/SearchPage.jsx
+++ client/src/components/SearchPage.jsx
@@ -6,12 +6,12 @@ import SearchFilters from './SearchFilters'
 import ResultViewer from './ResultViewer'
 
 export default function SearchPage() {
-
+    // immoweb color theme : blue #3f6ea7 and green #6ad690
     return (
 
         <SearchContext.Provider value={useSearch()}>
             <Layout>
-                <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor: 'floralwhite', minHeight: '90px' }}>
+                <Layout.Header style={{ position: 'fixed', zIndex: 1, width: '100%', backgroundColor: '#d7dfda', minHeight: '90px' }}>
                     <SearchFilters/>
                 </Layout.Header>
                 <Layout.Content style={{marginTop: '90px', paddingLeft: '20px'}} >
