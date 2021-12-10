ba8d3c182a450be88420473bef5ffe68572e857d
Laurent Vleminckx <laurentvleminckx@gmail.com>
2021-12-11 00:08:53 +0100

    Bugfix 2

--- server/src/index.js
+++ server/src/index.js
@@ -33,7 +33,7 @@
     //if(process.env.NODE__ENV === 'production') {
         app.use(express.static('client/build'))
         app.get('*', (req, res) => {
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+            res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
         })
     //}
 
