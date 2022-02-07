6ce0d422b3edb7c2d0dceb99c15d7f293a75309f
Laurent Vleminckx <laurentvleminckx@gmail.com>
2022-02-07 22:20:34 +0100

    Improve mongo queries

--- server/src/resolvers/estates.js
+++ server/src/resolvers/estates.js
@@ -1,15 +1,16 @@
 const { EstateModel } = require('../models/estates')
 const { queryHasField } = require('./utils')
+const _ = require('lodash')
 
 // Useful building blocks for MongoDB aggregation pipeline
 const filter = (filters) => !filters || !filters.length ? null : filters.length > 1 ? { $match: {$and: filters} } : { $match: filters[0] }
 const sort = (orderBy) => orderBy ? { $sort: orderBy } : null
-const skip = (offset) => offset ? { $skip: offset + 1 } : null
+const skip = (offset) => offset ? { $skip: offset } : null
 const trim = (limit) => limit >= 0 ? { $limit: limit } : null
 const count = () => ({ $count: 'totalCount'})
 const limitToValue = (field, treshold, order) => ({ $match: {[field]: {[order ? '$lte' : '$gte']: treshold}} })
 const onlyKeepField = (field) => field ? { $project: {[field]: 1} } : null
-//const onlyKeepFields = (fields) => fields && fields.length ? { $project: Object.fromEntries(fields.map(f => [f, 1])) } : null
+const onlyKeepFields = (fields) => fields && fields.length ? { $project: Object.fromEntries(fields.map(f => [f, 1])) } : null
 
 const pickMostRecentByImmowebCode = () => ([
     { $sort: { immowebCode: 1, lastModificationDate: -1 } },
@@ -23,8 +24,9 @@ const paginatedResult = (offset, limit) => ({ $facet: {
 
 // Useful to apply pipeline
 function applyPipeline(pipeline) {
-    console.debug('Performing MongoDB aggregation')
-    console.debug(JSON.stringify(pipeline))
+    console.debug('Performing the following MongoDB aggregation :')
+    pipeline.forEach(stage => console.debug('   ' + JSON.stringify(stage, null, 0)))
+    console.debug()
     return EstateModel.aggregate(pipeline.filter(e=>e)).exec().catch(e => {
         if(e.codeName === 'QueryExceededMemoryLimitNoDiskUseAllowed') {
             throw new Error('↑ Too much to load from database ! Please refine your search criterias ↑')
@@ -59,38 +61,34 @@ async function findEstates(filtersInput, orderByInput, fields/*Input*/, offset, 
     const orderBy = mapSorterToMongo(orderByInput)
     const orderByField = Object.entries(orderBy)[0][0]
 
-    let results = []
-    // if we have both sorting and pagination, we should avoid sorting the whole result set before paginating
-    // instead (if filtering is on a field ascending), we find the max value of that field
-    // and we should first keep only the values less than that, then sort that smaller result set, then paginate
-    const shouldFilterBeforeSort = orderBy && limit
-    let minMaxValue = 0
-    if(shouldFilterBeforeSort) {
-        minMaxValue = await applyPipeline([
-            filter(filters),
-            onlyKeepField(orderByField),
-            sort(orderBy),
-            skip(offset),
-            trim(1)
-        ])
-        minMaxValue = minMaxValue[0][orderByField]
-    }
-
-    results = (await applyPipeline([
-        filter(filters),
-        //onlyKeepFields(fields),
-        shouldFilterBeforeSort && limitToValue(orderByField, minMaxValue, orderBy[orderByField]),
-        ...pickMostRecentByImmowebCode(),
-        sort(orderBy),
-        paginatedResult(offset, limit),
-    ]))[0]
-    if(results.totalCount && results.totalCount[0] && results.totalCount[0].totalCount) results.totalCount = results.totalCount[0].totalCount
+    let aggregation = (await applyPipeline([
+        filter(filters), // D A C H G B a E
+        onlyKeepFields(['immowebCode', orderByField, 'lastModificationDate']),
+        ...pickMostRecentByImmowebCode(), // D A C H G B E,
+        onlyKeepFields(['immowebCode', orderByField]),
+        sort(orderBy), // A B C D E G H     {_id: abcf7F4Cba, immowebCode: 3892137, lastModificationDate: 2021_32_1209°}
+        {$facet: {
+            totalCount: [{$count: 'count'}],
+            ids: [
+                onlyKeepField('_id'),   // {_id: abdkfzefh}
+                skip(offset+1),
+                trim(limit)
+            ]
+        }}
+    ]))
+    let totalCount = aggregation[0].totalCount[0].count
+    let page = await applyPipeline([
+        filter([{_id: {$in: aggregation[0].ids.map(e => e._id)}}]),
+        sort(orderBy)
+    ])
+    
+    let results = {totalCount, page}
 
     // Lazy fetch missing fields (TODO: could be done in aggregation pipeline ?)
     if(queryHasField(fields, 'Estate', 'isLiked')) {
         if(!user) throw new Error('Cannot fetch liked estates since no user is logged in')
         results = {
-            totalCount: results.totalCount,
+            ...results,
             page: results.page.map(e => ({
                 ...e,
                 isLiked: user.likedEstates.includes(e.immowebCode)
@@ -100,7 +98,7 @@ async function findEstates(filtersInput, orderByInput, fields/*Input*/, offset, 
     if(queryHasField(fields, 'Estate', 'isVisited')) {
         if(!user) throw new Error('Cannot fetch visited estates since no user is logged in')
         results = {
-            totalCount: results.totalCount,
+            ...results,
             page: results.page.map(e => ({
                 ...e,
                 isVisited: user.visitedEstates.includes(e.immowebCode)
